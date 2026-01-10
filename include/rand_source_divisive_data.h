e3a87198c1e807d0ec6f7b2c1b8ec7685633a863
alic3 <alic3dev@gmail.com>
2026-01-10 03:08:14 -0800

    `rand_source_divisive_data`:add_reseed_methods_and_byte_length_increase

--- include/rand_source_divisive_data.h
+++ include/rand_source_divisive_data.h
@@ -2,7 +2,8 @@
 #define __rand_source_divisive_data_h
 
 #define rand_source_divisive_length_generation_distributions 100
-#define rand_source_divisive_bytes_seed_default 256
+
+#define rand_source_divisive_bytes_seed_default 4096
 
 struct rand_source_divisive_data {
   unsigned char generation_distributions[
@@ -30,6 +31,26 @@ void rand_source_divisive_data_seed_generate(
   unsigned short int
 );
 
+void rand_source_divisive_data_reseed(
+  struct rand_source_divisive_data*
+);
+
+void rand_source_divisive_data_reseed_additive(
+  struct rand_source_divisive_data*
+);
+
+void rand_source_divisive_data_reseed_with_seed(
+  struct rand_source_divisive_data*,
+  unsigned char*,
+  unsigned short int
+);
+
+void rand_source_divisive_data_reseed_with_seed_additive(
+  struct rand_source_divisive_data*,
+  unsigned char*,
+  unsigned short int
+);
+
 void rand_source_divisive_data_initialize_seeded(
   struct rand_source_divisive_data*,
   unsigned char*,
@@ -37,6 +58,10 @@ void rand_source_divisive_data_initialize_seeded(
 );
 
 void rand_source_divisive_data_initialize_preseeded(
+  struct rand_source_divisive_data*
+);
+
+void rand_source_divisive_data_initialize_multiplier_value(
   struct rand_source_divisive_data*
 );
 
